a0a8bdb393327e1809931dc434776a736d8da419
Lance <Lance@lance.com>
2026-01-13 16:51:19 +0800

    change the count

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -13,8 +13,8 @@ function UseEffect() {
       <h3>useEffect Counter</h3>
       <p className={count % 2 === 0 ? "counter-even" : "counter-odd"}>{count}</p>
       <div className="button-group">
-        <button className="increment-btn" onClick={() => setCount(count + 1)}>Increment</button>
-        <button className="decrement-btn" onClick={() => setCount(count - 1)}>Decrement</button>
+        <button className="increment-btn" onClick={() => setCount(count => count + 1)}>Increment</button>
+        <button className="decrement-btn" onClick={() => setCount(count => count - 1)}>Decrement</button>
       </div>
     </div>
   );
